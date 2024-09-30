ea8266c6cb303c3128d20dcdb3172c05d2b30c85
boubnan <hajar.boubnan@student.vinci.be>
2024-09-30 17:05:38 +0200

    new: ex1.2

---
+++ exercices/1.2/types.ts
@@ -0,0 +1,34 @@
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+interface Drink {
+  id: number;
+  title: string;
+  image: string;
+  volume: number;
+  price: number;
+}
+
+type NewDrink = Omit<Drink, "id">;
+
+type NewPizza = Omit<Pizza, "id">;
+
+export type { Pizza, NewPizza, PizzaToUpdate, Film,Drink, NewDrink};
