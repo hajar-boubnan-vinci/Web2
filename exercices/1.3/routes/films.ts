f881028ea2eb685207c7766068987ca2a53e02d8
boubnan <hajar.boubnan@student.vinci.be>
2024-10-04 23:13:59 +0200

    new: ex1.7

--- exercices/1.3/routes/films.ts
+++ exercices/1.3/routes/films.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import path from "node:path";
+import { parse, serialize } from "../utils/json";
 import { Film, NewFilm } from "../types";
 
-const films: Film[] = [
+// Définir le chemin vers le fichier JSON
+const jsonDbPath = path.join(__dirname, "/../data/films.json");
+
+const defaultFilms: Film[] = [
   {
     id: 1,
     title: "Inception",
@@ -42,6 +47,7 @@ interface UpdateFilmBody {
 
 // READ ALL FILTERED
 router.get("/", (req, res) => {
+  const films = parse(jsonDbPath, defaultFilms);
   const minDuration = req.query['minimum-duration'] ? Number(req.query['minimum-duration']) : null;
   if (minDuration !== null && (isNaN(minDuration) || minDuration <= 0)) {
     return res.status(400).json({ error: "Wrong minimum duration" });
@@ -61,6 +67,7 @@ router.get("/:id", (req, res) => {
     return res.status(400).json({ error: "Invalid ID" });
   }
 
+  const films = parse(jsonDbPath, defaultFilms);
   const film = films.find(f => f.id === id);
   if (!film) {
     return res.status(404).json({ error: "Film not found" });
@@ -92,6 +99,8 @@ router.post("/", (req, res) => {
   }
   const { title, director, duration, budget, description, imageUrl } = body as NewFilm;
 
+  const films = parse(jsonDbPath, defaultFilms);
+
   // Vérifier les doublons
   const existingFilm = films.find(f => f.title === title && f.director === director);
   if (existingFilm) {
@@ -111,23 +120,27 @@ router.post("/", (req, res) => {
   };
 
   films.push(newFilm);
+  serialize(jsonDbPath, films);
   return res.status(201).json(newFilm);
 });
 
 // DELETE ONE
 router.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const films = parse(jsonDbPath, defaultFilms);
   const index = films.findIndex((film) => film.id === id);
   if (index === -1) {
     return res.sendStatus(404);
   }
   const deletedFilm = films.splice(index, 1);
+  serialize(jsonDbPath, films);
   return res.json(deletedFilm);
 });
 
 // PATCH ONE
 router.patch("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const films = parse(jsonDbPath, defaultFilms);
   const film = films.find((film) => film.id === id);
   if (!film) {
     return res.sendStatus(404);
@@ -155,6 +168,7 @@ router.patch("/:id", (req, res) => {
     film.duration = duration;
   }
 
+  serialize(jsonDbPath, films);
   return res.json(film);
 });
 
@@ -170,14 +184,17 @@ router.put("/:id", (req, res) => {
     return res.sendStatus(400);
   }
 
+  const films = parse(jsonDbPath, defaultFilms);
   const filmIndex = films.findIndex((film) => film.id === id);
   const newFilm = { id, title, director, budget, duration };
 
   if (filmIndex === -1) {
     films.push(newFilm);
+    serialize(jsonDbPath, films);
     return res.status(201).json(newFilm);
   } else {
     films[filmIndex] = newFilm;
+    serialize(jsonDbPath, films);
     return res.json(newFilm);
   }
 });
